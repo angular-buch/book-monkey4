e6cc7289ee9a1e59720c0cdbde1a235efe3415fa
The Buildbot <buildbot@angular2buch.de>
2020-04-26 09:05:57 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-schule/book-monkey4-src/commit/743bf9c463cb1199da0f09368f908d21bc3626b5
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey4-src/358

--- src/app/book-monkey/iteration-2/di/shared/book-store.service.ts
+++ src/app/book-monkey/iteration-2/di/shared/book-store.service.ts
@@ -11,11 +11,11 @@ export class BookStoreService {
   constructor() {
     this.books = [
       {
-        isbn: '9783864906466',
+        isbn: '9783864907791',
         title: 'Angular',
         authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Koppenhagen'],
-        published: new Date(2019, 4, 30),
-        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practices - mit NativeScript und NgRx',
+        published: new Date(2020, 6, 1),
+        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practices – inklusive RxJS, NgRx & PWA',
         rating: 5,
         thumbnails: [{
           url: 'https://ng-buch.de/buch1.jpg',
@@ -24,17 +24,17 @@ export class BookStoreService {
         description: 'Die Autoren führen Sie mit einem anspruchsvollen Beispielprojekt durch die Welt von Angular...'
       },
       {
-        isbn: '9783864903274',
+        isbn: '9783864905520',
         title: 'React',
         authors: ['Oliver Zeigermann', 'Nils Hartmann'],
-        published: new Date(2016, 6, 17),
+        published: new Date(2019, 11, 12),
         subtitle: 'Die praktische Einführung in React, React Router und Redux',
         rating: 3,
         thumbnails: [{
           url: 'https://ng-buch.de/buch2.jpg',
           title: 'Buchcover'
         }],
-        description: 'React ist ein JavaScript-Framework zur Entwicklung von Benutzeroberflächen...'
+        description: 'Das bewährte und umfassende Praxisbuch zu React...'
       }
     ];
   }
