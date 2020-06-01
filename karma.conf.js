a8933aab7b2044226216c0270729440832f0503e
The Buildbot <buildbot@angular2buch.de>
2020-06-01 12:04:49 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-schule/book-monkey4-src/commit/2db2f0e2ebfe803fa4efe8f5145a7deefa4335c8
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey4-src/504

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/BookMonkey'),
+      dir: require('path').join(__dirname, './coverage/book-monkey'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
